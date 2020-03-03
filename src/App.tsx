f8b3797296b61918d4dbe2747320ab59f5eb630d
An Tran <an.tran@canaldigital.com>
2020-03-03 13:34:20 +0100

    Added Cell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cell from './components/Cell';
 
 function App() {
+  const size = 5;
+
+  function renderCells() {
+    const cells: JSX.Element[] = [];
+
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        cells.push(<Cell key={i * size + j * size} />);
+      }
+    }
+    return cells;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid">{renderCells()}</div>
     </div>
   );
 }
---
+++ src/components/Cell.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Cell.css';
+
+const Cell = () => {
+  return <div className="grid-cell"></div>;
+};
+
+export default Cell;
